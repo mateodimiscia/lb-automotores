01a758d4419abe753d951df41fc3733e381e6692
Mateo Di Miscia <dimisciamateo@gmail.com>
2022-11-23 17:48:59 -0300

    Bootstrap aplicado

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,46 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+<<<<<<< HEAD
+  it(`should have as title 'LBAutos'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('LBAutos');
+=======
+  it(`should have as title 'lbautosfirebase'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('lbautosfirebase');
+>>>>>>> f06e17c (Desarrollo front)
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+<<<<<<< HEAD
+    expect(compiled.querySelector('.content span')?.textContent).toContain('LBAutos app is running!');
+=======
+    expect(compiled.querySelector('.content span')?.textContent).toContain('lbautosfirebase app is running!');
+>>>>>>> f06e17c (Desarrollo front)
+  });
+});
---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+<<<<<<< HEAD
+  title = 'LB Automotores';
+=======
+  title = 'lbautosfirebase';
+>>>>>>> f06e17c (Desarrollo front)
+}
